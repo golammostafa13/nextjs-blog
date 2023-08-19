1277b9381b824b8d90e54b1824316975296adb6d
mostafarmstu <mostafarmstu@gmail.com>
2023-08-19 11:53:40 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey! I'm <span className={utilStyles.title}>Mostafa</span>. I'm a
+          software engineer from{" "}
+          <span className={utilStyles.title}>Bangladesh</span>. Very much
+          passionate on <span className={utilStyles.italic}>Next.js</span>,{" "}
+          <span className={utilStyles.italic}>Express.js</span> and{" "}
+          <span className={utilStyles.italic}>Postgres</span>.
+        </p>
+        <p>
+          You can contact me on{" "}
+          <a href="https://twitter.com/MostafaRmstu">Twitter</a>.
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [
+      './pages/**/*.{js,jsx}',
+      './components/**/*.{jsx}',
+      // For the best performance and to avoid false positives,
+      // be as specific as possible with your content configuration.
+    ],
+  }
